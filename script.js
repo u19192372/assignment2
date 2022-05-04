9cb52984833d6395a65fb84cf04e46fd84721961
u19192372 <102986611+u19192372@users.noreply.github.com>
2022-05-04 21:23:51 +0200

    ivys first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let movies = [
+    {
+    id: number, //a unique identifier for this movie
+    title: string, //the title of this movie
+    director: string, //the name of the director of this movie
+    runtime: string, //the runtime of this movie
+    release_year: number, //the year this movie was released
+    description: string, //a description of this movie
+    poster_url: string, //a URL or file path to an image of the poster for this movie
+    cinema_number: number, //the number of the cinema this movie will be showing in
+    ticket_price: number, //the price of a single ticket for this movie
+    tickets_in_cart: number //the number of tickets that have been booked for this movie
+    }
+    ];
+
+    
+  
